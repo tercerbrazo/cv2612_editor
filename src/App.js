d5ea47c47ad028edd9677ae190b6789bad702f87
Diego Dorado <diegodorado@gmail.com>
2022-09-18 15:49:58 -0300

    Improve trbz styling

--- src/App.js
+++ src/App.js
@@ -5,13 +5,11 @@ import Midi from "./midi";
 import Scene from "./scene";
 import logo from "./logo.png";
 
-console.log(logo);
-
 const App = () => (
   <CV2612Provider>
     <div className="two-cols">
       <div className="col">
-        <img src={logo} height={"80px"} />
+        <img style={{ filter: "invert(1)" }} src={logo} height={"80px"} />
       </div>
       <div className="col">
         <h3 style={{ textAlign: "right" }}>CV-2612 Editor</h3>
