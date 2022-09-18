b50d8fced615a1bab69978df32f1aa5982732185
Diego Dorado <diegodorado@gmail.com>
2022-09-18 13:17:32 -0300

    First working version

--- src/App.js
+++ src/App.js
@@ -1,3 +1,15 @@
-export function App() {
-  return <h1>CV2612 EDITOR</h1>;
-}
+import React from "react";
+import { CV2612Provider } from "./context";
+import "./styles.sass";
+import Midi from "./midi";
+import Scene from "./scene";
+
+const App = () => (
+  <CV2612Provider>
+    <h3>CV-2612 Editor</h3>
+    <Midi />
+    <Scene />
+  </CV2612Provider>
+);
+
+export default App;
