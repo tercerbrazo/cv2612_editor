033214df90815f58fe22159b7f915288b7cbbef9
Diego Dorado <diegodorado@gmail.com>
2022-09-18 14:44:29 -0300

    Style as TRBZ

--- src/App.js
+++ src/App.js
@@ -3,10 +3,20 @@ import { CV2612Provider } from "./context";
 import "./styles.sass";
 import Midi from "./midi";
 import Scene from "./scene";
+import logo from "./logo.png";
+
+console.log(logo);
 
 const App = () => (
   <CV2612Provider>
-    <h3>CV-2612 Editor</h3>
+    <div className="two-cols">
+      <div className="col">
+        <img src={logo} height={"80px"} />
+      </div>
+      <div className="col">
+        <h3 style={{ textAlign: "right" }}>CV-2612 Editor</h3>
+      </div>
+    </div>
     <Midi />
     <Scene />
   </CV2612Provider>
