571936d6407b4a18e5e007b59e1c979804f9c84d
Diego Dorado <diegodorado@gmail.com>
2022-09-29 16:09:35 -0300

    remove unused midi io code

--- src/midi-io.js
+++ src/midi-io.js
@@ -4,8 +4,6 @@ const state = {
 };
 
 const INTERVAL = 20;
-const cc_queue = new Map();
-const cc_cache = new Map();
 
 const sleep = (ms) => new Promise((resolve) => setTimeout(resolve, ms));
 
@@ -40,57 +38,11 @@ const init = async () => {
 };
 
 const refresh = () => {
-  const inputs = Array.from(state.ma.inputs.values());
   const outputs = Array.from(state.ma.outputs.values());
 
-  // inputs.forEach(m => (m.onmidimessage = msg => console.log(msg)))
-
   pub("midiStateChanged", {
-    inputs,
     outputs,
   });
-};
-
-//TODO: make a web woker to handle midi out message queue
-const sendCC_Old = async (channel, number, value) => {
-  if (!state.ma) return;
-  //throw new Error("No midiAcces object.")
-
-  const midiOut = state.ma.outputs.get(state.midiOutId);
-  if (!midiOut) return;
-  //throw new Error("No midi Out.")
-
-  const key = `${channel}-${number}`;
-
-  // TODO: enable cache properly
-  // do not cache above CC70
-  // if (cc_cache.has(key) && cc_cache.get(key) === value && number < 70) {
-  //   return
-  // }
-
-  cc_queue.set(key, value);
-
-  let index = cc_queue.size;
-  let start = performance.now();
-  //todo: change this awful sleep with a queue and a worker
-  await sleep(INTERVAL * index);
-
-  if (cc_queue.has(key)) {
-    const value = cc_queue.get(key);
-
-    cc_cache.set(key, value);
-    const msg = [0xb0 + channel, number, value];
-    midiOut.send(msg);
-
-    console.log(`CC ${channel}:${number} -> ${value}`);
-
-    cc_queue.delete(key);
-
-    let elapsed = performance.now() - start;
-    let done = cc_queue.size === 0;
-
-    pub("midiOutProgress", { elapsed, done });
-  }
 };
 
 let pendingCCs = 0;
